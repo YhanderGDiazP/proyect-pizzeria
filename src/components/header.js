00f6154569e320e0fe9bf105d39ebb2efc0aa609
YhanderGDiazP <201284@ids.upchiapas.edu.mx>
2023-03-11 22:47:14 -0600

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles/header.css'
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div className="body-header">
+                <h2>Esto sera el Header</h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/pages/inicio.js
@@ -0,0 +1,14 @@
+import Header from '../components/header';
+import './styles/inicio.css'
+
+function Inicio(){
+
+    return (
+        <div className='body-inicio'>
+            <Header />
+            <h2>Hola, soy el Inicio</h2>
+        </div>
+    );
+}
+
+export default Inicio;
